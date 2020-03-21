25fbee42ab631c313347af0d83a7d3ad7a46455c
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-21 10:35:30 +0530

    Create Home.js

---
+++ screens/Home.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import * as firebase from 'firebase'
+import {FormButton} from "../components/FormButton";
+import {Button} from "../components/Button";
+
+export default class Main extends React.Component {
+    state = {currentUser: null}
+
+    componentDidMount() {
+        const {currentUser} = firebase.auth()
+        this.setState({currentUser})
+    }
+
+    handleLogOut = () => {
+        firebase.auth().signOut().then(() => {
+            console.warn("Logged Out successfully")
+            // console.log("Logged Out successfully")
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        const {currentUser} = this.state
+        return (
+            <View style={styles.container}>
+                <Button onPress={this.handleLogOut}>Logout</Button>
+                <Text>
+                    Hi {currentUser && currentUser.email}!
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
