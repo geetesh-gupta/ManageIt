ea9c251a741e45a753c10522eecc1460739d6f4d
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-21 10:35:42 +0530

    Update useLinking.js

--- navigation/useLinking.js
+++ navigation/useLinking.js
@@ -1,18 +1,19 @@
-import { useLinking } from '@react-navigation/native';
-import { Linking } from 'expo';
+import {useLinking} from '@react-navigation/native';
+import {Linking} from 'expo';
 
-export default function(containerRef) {
-  return useLinking(containerRef, {
-    prefixes: [Linking.makeUrl('/')],
-    config: {
-      Root: {
-        path: 'root',
-        screens: {
-          Home: 'home',
-          Links: 'links',
-          Settings: 'settings',
+export default function (containerRef) {
+    return useLinking(containerRef, {
+        prefixes: [Linking.makeUrl('/')],
+        config: {
+            Root: {
+                path: 'root',
+                screens: {
+                    Home: 'home',
+                    Login: 'login',
+                    SignUp: 'signup',
+                    Loading: 'loading',
+                },
+            },
         },
-      },
-    },
-  });
+    });
 }
