83266ff3607bbd3ef17e926cbe6e1770d552cccb
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-21 17:51:12 +0530

    Create common firebase utils file

---
+++ assets/firebase.js
@@ -0,0 +1,85 @@
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+export const initFirebase = () => firebase.initializeApp(firebaseConfig);
+
+export const createFirebaseData = (
+  path,
+  value,
+  successCallback,
+  errorCallback
+) => {
+  firebase
+    .database()
+    .ref(path)
+    .push(value)
+    .then(res => successCallback(res))
+    .catch(err => errorCallback(err));
+};
+
+export const updateFirebaseData = (
+  path,
+  values,
+  successCallback,
+  errorCallback
+) => {
+  firebase
+    .database()
+    .ref(path)
+    .update(values)
+    .then(res => successCallback(res))
+    .catch(err => errorCallback(err));
+};
+
+export const loginFirebase = (
+  email,
+  password,
+  successCallback,
+  errorCallback
+) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(res => successCallback(res))
+    .catch(err => errorCallback(err));
+};
+
+export const logoutFirebase = (successCallback, errorCallback) => {
+  firebase
+    .auth()
+    .signOut()
+    .then(res => successCallback(res))
+    .catch(err => errorCallback(err));
+};
+
+export const signUpFirebase = (
+  email,
+  password,
+  successCallback,
+  errorCallback
+) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(res => successCallback(res))
+    .catch(err => errorCallback(err));
+};
+
+export const readFirebaseData = (
+  path,
+  eventType,
+  successCallback,
+  errorCallback
+) => {
+  firebase
+    .database()
+    .ref(path)
+    .on(eventType, snap => {
+      const data = snap.val() ? snap.val() : {};
+      successCallback(data);
+    });
+};
+
+export const authFirebase = () => {
+  return firebase.auth();
+};
