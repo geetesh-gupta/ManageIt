6009a2d78938967fdc90ccafd8a13f267025146f
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-22 12:18:03 +0530

    Create StyledText component

---
+++ components/StyledText.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+import PropTypes from "prop-types";
+import styles from "./styles";
+
+const StyledText = props => {
+  return <Text style={[styles.cardDesc, props.style]}>{props.children}</Text>;
+};
+
+StyledText.propTypes = {
+  style: PropTypes.object,
+  children: PropTypes.node
+};
+
+StyledText.defaultProps = {
+  style: {},
+  children: ""
+};
+
+export { StyledText };
