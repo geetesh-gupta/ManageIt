f2ccb4fabb3ee633167f85c51827cd24036c30ba
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-23 10:22:17 +0530

    Update CardItem.js

--- screens/CardItem.js
+++ screens/CardItem.js
@@ -1,27 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { Card } from "../components/Card";
 import { CardSection } from "../components/CardSection";
 import { StyledText } from "../components/StyledText";
-import {
-  readFirebaseData,
-  updateFirebaseData,
-  authFirebase
-} from "../assets/firebase";
+import { readFirebaseData, authFirebase } from "../assets/firebase";
+import { navigate } from "../components/RootNavigation";
 
 export default class CardItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: "", cardId: "", listId: "" };
+    this.state = { title: "", cardId: "" };
   }
 
   componentDidMount() {
-    const { cardId, listId } = this.props;
+    const { cardId } = this.props;
     const { currentUser } = authFirebase();
 
     this.setState({
-      listId,
       cardId
     });
 
@@ -39,35 +35,16 @@ export default class CardItem extends React.Component {
     );
   }
 
-  updateCard = title => {
-    const { currentUser } = authFirebase();
-
-    updateFirebaseData(
-      `${currentUser.uid}/cards/`,
-      {
-        [this.state.cardId]: {
-          title
-        }
-      },
-      () => console.log("Card updated", this.state.cardId),
-      err => console.log("Err:", err, "| Card not updated", this.state.cardId)
-    );
-  };
-
   render() {
-    const { title, cardId, listId } = this.state;
+    const { title, cardId } = this.state;
     return (
-      <Card style={styles.card}>
-        <CardSection>
-          <StyledText>{title}</StyledText>
-        </CardSection>
-        <CardSection>
-          <StyledText>{cardId}</StyledText>
-        </CardSection>
-        <CardSection>
-          <StyledText>{listId}</StyledText>
-        </CardSection>
-      </Card>
+      <TouchableOpacity onPress={() => navigate("CardDetails", { cardId })}>
+        <Card style={styles.card}>
+          <CardSection>
+            <StyledText>{title}</StyledText>
+          </CardSection>
+        </Card>
+      </TouchableOpacity>
     );
   }
 }
@@ -80,6 +57,5 @@ const styles = StyleSheet.create({
 });
 
 CardItem.propTypes = {
-  cardId: PropTypes.string.isRequired,
-  listId: PropTypes.string.isRequired
+  cardId: PropTypes.string.isRequired
 };
