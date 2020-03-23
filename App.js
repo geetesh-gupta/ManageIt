83c871716755f4117f746a6ea5ad45b6f88aa8b7
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-03-23 13:10:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { SplashScreen } from "expo";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import useLinking from "./navigation/useLinking";
@@ -44,9 +42,25 @@ export default function App(props) {
         setInitialNavigationState(await getInitialState());
 
         // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font
-        });
+        // await Font.loadAsync({
+        //   "Montserrat-Black": require("./assets/fonts/Montserrat-Black.ttf"),
+        //   "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+        //   "Montserrat-BoldItalic": require("./assets/fonts/Montserrat-BoldItalic.ttf"),
+        //   "Montserrat-ExtraBold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+        //   "Montserrat-ExtraBoldItalic": require("./assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
+        //   "Montserrat-ExtraLight": require("./assets/fonts/Montserrat-ExtraLight.ttf"),
+        //   "Montserrat-ExtraLightItalic": require("./assets/fonts/Montserrat-ExtraLightItalic.ttf"),
+        //   "Montserrat-Italic": require("./assets/fonts/Montserrat-Italic.ttf"),
+        //   "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
+        //   "Montserrat-LightItalic": require("./assets/fonts/Montserrat-LightItalic.ttf"),
+        //   "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+        //   "Montserrat-MediumItalic": require("./assets/fonts/Montserrat-MediumItalic.ttf"),
+        //   "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+        //   "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+        //   "Montserrat-SemiBoldItalic": require("./assets/fonts/Montserrat-SemiBoldItalic.ttf"),
+        //   "Montserrat-Thin": require("./assets/fonts/Montserrat-Thin.ttf"),
+        //   "Montserrat-ThinItalic": require("./assets/fonts/Montserrat-ThinItalic.ttf")
+        // });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
         console.warn(e);
